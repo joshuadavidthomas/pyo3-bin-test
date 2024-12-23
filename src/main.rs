6123da547242e93a7fec03bc94ae66b4de5fa05c
Josh <josh@joshthomas.dev>
2024-12-22 18:25:42 -0600

    add pyo3 dep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use pyo3::prelude::*;
+
+fn main() -> PyResult<()> {
+    Python::with_gil(|py| {
+        let sys = py.import("sys")?;
+        let version: String = sys.getattr("version")?.extract()?;
+        println!("Python version: {}", version);
+        Ok(())
+    })
 }
